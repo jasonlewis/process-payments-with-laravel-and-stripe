cd09c346980921d57abf08f349cd52fa89586bc9
Jason Lewis <jason.lewis1991@gmail.com>
2015-12-23 20:23:51 +1100

    Added the products and cart table migrations.
    Signed-off-by: Jason Lewis <jason.lewis1991@gmail.com>

---
+++ database/migrations/2015_12_23_092240_create_cart_table.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateCartTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('cart', function (Blueprint $table) {
+            $table->increments('id');
+            $table->integer('user_id');
+            $table->integer('product_id');
+            $table->integer('quantity');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::drop('cart');
+    }
+}
